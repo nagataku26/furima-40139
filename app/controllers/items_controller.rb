cbe21f8f66cf4af7d8bdbb885fb11500776b2b3b
nagataku26 <nagataku.biz@gmail.com>
2023-12-15 16:43:43 +0900

    商品出品ページ設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_delivery_id, :price).merge(user_id: current_user.id)
+  end
 end
