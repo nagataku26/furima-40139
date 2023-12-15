089d42064135a5305eb7025350e1cd3e20584f47
nagataku26 <nagataku.biz@gmail.com>
2023-12-15 14:27:32 +0900

    バリデートの修正、テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,11 +44,23 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordが半角英数字混合でないと登録できない' do
+      it 'passwordが半角数字のみでは登録できない' do
         @user.password = '111111'
         @user.password_confirmation = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+        expect(@user.errors.full_messages).to include('Password には半角の英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordが半角英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には半角の英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordに全角文字が含まれていると登録できない' do
+        @user.password = '11111ａ'
+        @user.password_confirmation = '11111ａ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には半角の英字と数字の両方を含めて設定してください')
       end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '11111a'
