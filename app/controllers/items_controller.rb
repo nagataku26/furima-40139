43892fd6ce65fe151df39e3faaf0c3047e485ff7
nagataku26 <nagataku.biz@gmail.com>
2023-12-15 17:05:43 +0900

    画像保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_delivery_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
