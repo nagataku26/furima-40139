2f427e71e4f9ca3f087136e9fa359c77a2785b43
nagataku26 <nagataku.biz@gmail.com>
2023-12-18 09:09:26 +0900

    カテゴリー機能追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,23 @@
 class Item < ApplicationRecord
-  validates :item_name, presence: true
-  validates :item_description, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :days_delivery_id, presence: true
-  validates :price, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :days_delivery
 
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :status_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :days_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, 
+  format: { with: /\}
+  format: { with: /\A[0-9]+\z/, message: "は半角数字のみ入力してください" }
+
 end
