71ea9370117c43960d4ee78b8aac2637edc9bad3
nagataku26 <nagataku.biz@gmail.com>
2023-12-20 15:24:42 +0900

    レビュー内容の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,6 +41,8 @@ class ItemsController < ApplicationController
     if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
+    else
+      render :show, status: :unprocessable_entity
     end
   end
 
