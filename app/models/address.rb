c9d06ec2fb009006ab96de01c70497c77f540c55
nagataku26 <nagataku.biz@gmail.com>
2023-12-25 17:55:09 +0900

    住所情報入力欄の設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,11 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
   belongs_to :order
 
   validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'はハイフンを含む半角文字で入力してください' }
-  validates :prefecture_id, resence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city, resence: true
   validates :block, resence: true
   validates :phone_number, length: { minimum: 10, maximum: 11 }, numericality: { only_integer: true}
