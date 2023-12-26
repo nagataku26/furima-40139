73610f839d450da4d422131a9077f618b1d5eeee
nagataku26 <nagataku.biz@gmail.com>
2023-12-26 23:49:50 +0900

    テストコード記述

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+  end
+
+  describe '購入情報の保存' do
+    context '商品購入ができる場合' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@order_address).to be_valid
+      end
+      it 'building_nameは空でも保存できる' do
+        @order_address.building_name = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '商品購入ができない場合' do
+      it 'tokenが空では保存できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postal_codeが空では保存できない' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeに-が含まれていないと保存できない' do
+        @order_address.postal_code = '1111111'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code はハイフンを含む半角文字で入力してください")
+      end
+      it 'postal_codeが全角数字では保存できない' do
+        @order_address.postal_code = '１１１-１１１１'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code はハイフンを含む半角文字で入力してください")
+      end
+      it 'postal_codeが「3桁ハイフン4桁」でないと保存できない' do
+        @order_address.postal_code = '1111-111'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code はハイフンを含む半角文字で入力してください")
+      end
+      it 'prefecture_idが空では保存できない' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'prefecture_idが「---」が選択されている場合は保存できない' do
+        @order_address.prefecture_id = '1'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'cityが空では保存できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'blockが空では保存できない' do
+        @order_address.block = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Block can't be blank")
+      end
+      it 'phone_numberが空では保存できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが全角数字では保存できない' do
+        @order_address.phone_number = '０９０１２３４５６７８'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number は10桁または11桁の半角数字で入力してください")
+      end
+      it 'phone_numberに-が含まれていると保存できない' do
+        @order_address.phone_number = '090-1234-5678'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number は10桁または11桁の半角数字で入力してください")
+      end
+      it 'phone_numberが10文字以下では保存できない' do
+        @order_address.phone_number = '090123456'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number は10桁または11桁の半角数字で入力してください")
+      end
+      it 'phone_numberが11文字以上では保存できない' do
+        @order_address.phone_number = '090123456789'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number は10桁または11桁の半角数字で入力してください")
+      end
+      it 'userが紐づいていないと保存できない' do
+        @order_address.user_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐づいていないと保存できない' do
+        @order_address.item_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
+end
